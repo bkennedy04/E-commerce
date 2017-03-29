215b61ff0602671c6f4603015d66bf7ec0209b3b
bkennedy04 <bkennedy04@bellarmine.edu>
2017-03-28 20:38:24 -0400

    Added more entries to db

--- website/trending.php
+++ website/trending.php
@@ -14,7 +14,7 @@ if ($result->num_rows > 0) {
 <div class="col-sm-2">
   <div class="thumbnail">
     <img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
-    <span class="badge" style="font-weight: bold; position: absolute; margin: -120px 0 0 5px; opacity: 0.75">
+    <span class="badge" style="font-weight: bold; position: absolute; margin: -60% 0 0 5%; opacity: 0.75">
     <?php 
     echo "#" . $rank; 
     if($rank == 1)
@@ -25,7 +25,7 @@ if ($result->num_rows > 0) {
     </span>
     <div class="caption">
       <h3><?php echo $row["title"]; ?></h3>
-      <p>This looks good!!!!!!!!!</p>
+      <p></p>
       <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
     </div>
   </div>
