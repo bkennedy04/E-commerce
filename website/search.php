a929f3fb350efab2c977fcb73dc0986076614e6b
bkennedy04 <bkennedy04@bellarmine.edu>
2017-04-13 00:47:19 -0400

    Updated user manual. Took out normalization.
    Apparently Bootstrap already normalizes across browsers.

--- website/search.php
+++ website/search.php
@@ -7,22 +7,21 @@ include "db_connect.php";
 $query9 = "SELECT DISTINCT title, recipe.recipe_id, image_url FROM recipe JOIN ingredients ON ingredients.recipe_id = recipe.recipe_id WHERE LOWER(ingredient_name) LIKE LOWER('%".$_POST["search"]."%')";
 $result9 = $conn->query($query9);
 //if search is for recipe with certain ingredient:
-
 	if ($result9->num_rows > 0) {
 		while($row = $result9->fetch_array()) {
 
 ?>
 
-<div class="col-sm-2">
-  <div class="thumbnail">
-    <img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
-    <div class="caption">
-      <h3><?php echo $row["title"]; ?></h3>
-      <p></p>
-      <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
-    </div>
-  </div>
-</div>
+			<div id="grocery_item" class="col-md-2">
+			  <div class="thumbnail">
+				<img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
+				<div class="caption">
+				  <h3><?php echo $row["title"]; ?></h3>
+				  <p></p>
+				  <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
+				</div>
+			  </div>
+			</div>
 
 <?php 
 		}
@@ -39,29 +38,29 @@ $result9 = $conn->query($query9);
 
 ?>
 
-<div class="col-sm-2">
-  <div class="thumbnail">
-    <img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
-    <div class="caption">
-      <h3><?php echo $row["title"]; ?></h3>
-      <p></p>
-      <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
-    </div>
-  </div>
-</div>
+					<div id="grocery_item" class="col-md-2">
+					  <div class="thumbnail">
+						<img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
+						<div class="caption">
+						  <h3><?php echo $row["title"]; ?></h3>
+						  <p></p>
+						  <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
+						</div>
+					  </div>
+					</div>
 
 <?php 
 				}
 			}
 	
 	}
-	
+
 $sql1 = "SELECT * FROM grocery WHERE LOWER(title) LIKE LOWER('%".$_POST["search"]."%')";
 $result1 = $conn->query($sql1);
 //if search is for a grocery item:
 ?>
-	
-	<div id="groceries" class="col-sm-10">
+
+	<div id="groceries" class="col-md-10">
     <div class="row">
 	
 <?php
@@ -71,21 +70,19 @@ $result1 = $conn->query($sql1);
 	
 
 ?>
-      <div id="grocery_item" class="col-md-2">
-        <div class="thumbnail">
-          <img src="<?php echo $row["img"];?>" alt="<?php echo $row["title"];?>" class="img-rounded" />
-            <h3 style="text-align: center"><?php echo $row["title"];?></h3>
-            <div class="btn-group btn-block">
-			  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-				$<?php echo $row["price"];?><span class="caret"></span>
-			  </button>
-			  <ul class="dropdown-menu">
-				<li><a href="#">Add to Cart</a></li>
-				<!--li role="separator" class="divider"></li>
-				<li><a href="#">Separated link</a></li-->
-			  </ul>
+		<div id="grocery_item" class="col-md-2">
+			<div class="thumbnail">
+				<img src="<?php echo $row["img"];?>" alt="<?php echo $row["title"];?>" class="img-rounded" />
+				<h3 style="text-align: center"><?php echo $row["title"];?></h3>
+				<div class="btn-group btn-block">
+				  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+					$<?php echo $row["price"];?><span class="caret"></span>
+				  </button>
+				  <ul class="dropdown-menu">
+					<li><a href="#">Add to Cart</a></li>
+				  </ul>
+				</div>
 			</div>
-          </div>
         </div>
 <?php 
 
@@ -98,7 +95,7 @@ $result1 = $conn->query($sql1);
 	
 <?php
 
-
+//-----------------------------------------------------------------------------------------
 $query3 = "SELECT * FROM categories JOIN recipe ON categories.recipe_id = recipe.recipe_id WHERE LOWER(category) LIKE LOWER('".$_POST["search"]."')";
 $result3 = $conn->query($query3);
 //if search is for category of recipes:
@@ -116,16 +113,16 @@ $result3 = $conn->query($query3);
 ?>
 
 
-<div class="col-sm-2">
-  <div class="thumbnail">
-    <img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
-    <div class="caption">
-      <h3><?php echo $row["title"]; ?></h3>
-      <p></p>
-      <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
-    </div>
-  </div>
-</div>
+			<div class="col-md-2">
+			  <div class="thumbnail">
+				<img src="<?php echo $row["image_url"] ?>" alt="<?php echo $row["title"]; ?>" class="img-rounded" />
+				<div class="caption">
+				  <h3><?php echo $row["title"]; ?></h3>
+				  <p></p>
+				  <div class="go-to-recipe-btn"><a href="newexample.php?recipe_id=<?php echo $row["recipe_id"]; ?>" class="btn btn-primary" role="button">Go To Recipe</a></div>
+				</div>
+			  </div>
+			</div>
 
 <?php 
 
