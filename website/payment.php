08eed764da9f5325830cff64454ac5a5490f0116
cblevins03 <cblevins03@bellarmine.edu>
2017-04-19 17:58:33 -0400

    finished updating the final checkout page

--- website/payment.php
+++ website/payment.php
@@ -33,29 +33,29 @@
 				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="(502) 555-4567">
 			  </div>
 			  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding-bottom:15px">			  
-				  <button type="button" class="btn btn-primary pull-left" data-toggle="collapse" data-target="#card">Pay With Card</button>
+				  <button type="button" class="btn btn-default pull-left" data-toggle="collapse" data-target="#card">Pay With Card : <img alt="Credit Card Logos" title="Credit Card Logos" src="http://www.credit-card-logos.com/images/multiple_credit-card-logos-1/credit_card_logos_17.gif" width="235" height="35" border="0" /></button>
 					  <div id="card" class="collapse">
 						  <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-lg-offset-1">
 							  <form>
 								  <div class="form-group">
 									<label for="exampleInputEmail1">Card Number</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="YYYY-MM-DD">
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Card Number">
 								  </div>
 								  <div class="form-group">
 									<label for="exampleInputEmail1">Expiration Date</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ex: 9:00am">
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="MM/YY">
 								  </div>
 								  <div class="form-group">
 									<label for="exampleInputEmail1">Security Code</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Hospital">
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Security Code">
 								  </div>
 								  <div class="form-group">
 									<label for="exampleInputEmail1">Name On Card</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Location">
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Name">
 								  </div>
 								  <div class="form-group">
 									<label for="exampleInputPassword1">Billing Address</label>
-									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Doctor">
+									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Address">
 								  </div>
 							  </form>
 						  </div>
@@ -67,57 +67,19 @@
 						  <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-lg-offset-1">
 							  <form>
 								  <div class="form-group">
-									<label for="exampleInputEmail1">Card Number</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="YYYY-MM-DD">
+									<label for="exampleInputEmail1">Username</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Username">
 								  </div>
 								  <div class="form-group">
-									<label for="exampleInputEmail1">Expiration Date</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ex: 9:00am">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputEmail1">Security Code</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Hospital">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputEmail1">Name On Card</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Location">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputPassword1">Billing Address</label>
-									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Doctor">
+									<label for="exampleInputEmail1">Password</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Password">
 								  </div>
 							  </form>
 						  </div>
 					  </div>
 					  </div>
 				  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding-bottom:15px">
-					  <button type="button" class="btn btn-primary pull-left" data-toggle="collapse" data-target="#paypal">Paypal</button>
-					  <div id="paypal" class="collapse">
-						  <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-lg-offset-1">
-							  <form>
-								  <div class="form-group">
-									<label for="exampleInputEmail1">Card Number</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="YYYY-MM-DD">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputEmail1">Expiration Date</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ex: 9:00am">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputEmail1">Security Code</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Hospital">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputEmail1">Name On Card</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Location">
-								  </div>
-								  <div class="form-group">
-									<label for="exampleInputPassword1">Billing Address</label>
-									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Doctor">
-								  </div>
-							  </form>
-						  </div>
-					  </div>
+					  <a href="test.php"><img src="https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif" align="left" style="margin-right:7px;"></a>
 				  </div>
 			</form>
 			<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-lg-offset-1" style="text-align:center;">
